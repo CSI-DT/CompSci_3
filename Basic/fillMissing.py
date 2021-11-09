c1a57c3a5bda3b1cd00263e0d053b6fd5e0f67f6
arfo6529 <93907406+arfo6529@users.noreply.github.com>
2021-11-09 17:08:18 +0100

    Update fillMissing.py
    Added makima interpolation

--- Basic/fillMissing.py
+++ Basic/fillMissing.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import numpy as np
+import makimaInterpolator
 
+# Fill missing data of a dataframe between start_time and end_time
 def fillMissing(ind_data, start_time, end_time):
     ind_data.reset_index(drop=True)
     newTimes = np.arange(start_time, end_time+1, 1)
-        
+
     # Declare output dataframe
     filled_data = pd.DataFrame(columns = ["ID", "Time", "x", "y"])
     filled_data["Time"] = newTimes
@@ -28,7 +30,25 @@ def fillMissing(ind_data, start_time, end_time):
     filled_data.loc[:first_non_nan_x, "x"] = filled_data["x"].iloc[first_non_nan_x]
     filled_data.loc[:first_non_nan_y, "y"] = filled_data["y"].iloc[first_non_nan_y]
     
-    # # Fill nan values with linear interpolation...
-    filled_data = filled_data.astype(float).interpolate()
+    # # Fill nan values with makima interpolation...
+    filled_data.iloc[filled_data["x"].dropna().to_frame().index]["x"].values
+    filled_data.iloc[filled_data["y"].dropna().to_frame().index]["y"].values
+    
+    # non-nan x_times, y_times, values x, values y
+    nnan_times_x, nnan_times_y, vx, vy = filled_data.iloc[filled_data["x"].dropna().to_frame().index]["Time"].values, filled_data.iloc[filled_data["y"].dropna().to_frame().index]["Time"].values, filled_data.iloc[filled_data["x"].dropna().to_frame().index]["x"].values, filled_data.iloc[filled_data["y"].dropna().to_frame().index]["y"].values
+   
+    # nan rows + times x and y
+    nan_rows_x = filled_data[filled_data["x"].isnull().to_frame().any(axis=1)].index
+    nan_rows_y = filled_data[filled_data["y"].isnull().to_frame().any(axis=1)].index
+    nan_times_x = filled_data.loc[nan_rows_x, :]["Time"].values
+    nan_times_y = filled_data.loc[nan_rows_y, :]["Time"].values
+        
+    # Interpolate
+    vxq = makimaInterpolator.makima(nnan_times_x, vx, nan_times_x)    
+    vyq = makimaInterpolator.makima(nnan_times_y, vy, nan_times_y)    
+    
+    filled_data.loc[nan_rows_x, 'x'] = vxq
+    filled_data.loc[nan_rows_y, 'y'] = vyq
+    
     
     return filled_data
