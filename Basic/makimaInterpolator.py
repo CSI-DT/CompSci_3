c57373777d97e1afa97a7877dea7ac4f94d325fb
arfo6529 <93907406+arfo6529@users.noreply.github.com>
2021-11-10 10:05:34 +0100

    Update makimaInterpolator.py
    Initial optimisations performed

--- Basic/makimaInterpolator.py
+++ Basic/makimaInterpolator.py
@@ -1,8 +1,17 @@
 import numpy as np
 import bisect
+import time
 
-# Calculates derivatives d of makima formula 
-def makimaSlopes(delta):
+def d_calc(w1, w2, delta1, delta2):
+    return (w1*delta1 + w2*delta2)/(w1 + w2) if (w1 + w2 != 0) else 0
+
+# Returns interpolation evaluated at points xq based on points (x, v)
+def makima(x, v, xq):
+    h = np.diff(x)
+    delta = np.diff(v) / h
+    # time1 = time.perf_counter()
+
+    # Calculate slopes d_i    
     n = len(delta) + 1 # Number of grid points
     padded_delta = np.empty(len(delta) + 4, dtype = float)
     padded_delta[2:(2 + len(delta))] = delta
@@ -20,47 +29,20 @@ def makimaSlopes(delta):
     for i in np.arange(2, n+2):
         w1[i-2] = abs(padded_delta[i+1] - padded_delta[i]) + 1/2 * abs(padded_delta[i+1] + padded_delta[i])
         w2[i-2] = abs(padded_delta[i-1] - padded_delta[i-2]) + 1/2 * abs(padded_delta[i-1] + padded_delta[i-2])
-        d[i-2] = w1[i-2]/(w1[i-2] + w2[i-2]) * padded_delta[i-1] + w2[i-2]/(w1[i-2] + w2[i-2]) * padded_delta[i]
+        d[i-2] = d_calc(w1[i-2], w2[i-2], padded_delta[i-1], padded_delta[i])
+        
+    # print("Slopes calc took ", time.perf_counter() - time1, " s")
     
-    # Replace nan values of d with 0. Happens if five consequtive constant values
-    # in v
-    np.nan_to_num(d, copy=False)
-
-    return d
-    
-# Returns interpolation evaluated at points xq based on points (x, v)
-def makima(x, v, xq):
-    h = np.diff(x)
-    delta = np.diff(v) / h
-    slopes = makimaSlopes(delta) # d vector from internet
-    vq = np.empty(len(xq), dtype = float)
     # Now we should find values vq for xq using the formula for 
     # polynomial Hermitian cubic interpolation 
-    # Loop through xq, find in between which x values it exists
-    # find the vq of xq using the formula with the two padding x values
-    # maybe could handle endpoints better!
-    for i in np.arange(0, len(xq)):
+    # time2 = time.perf_counter()
+    vq = np.empty(len(xq), dtype = float)
+    for i in np.arange(0, len(xq) - len(xq[np.where(np.greater(xq, x[-1]))])): # Loop over all xq smaller than largest x
         low_x_index = bisect.bisect_left(x, xq[i]) - 1 # Find index of interval lower x
-        if (low_x_index != len(x)-1): # If the xq is not outside x
-            hk = x[low_x_index + 1] - x[low_x_index] # Interval length
-            s = xq[i] - x[low_x_index]         
-            vq[i] = (3*hk*s**2 - 2*s**3)/(hk**3)*v[low_x_index + 1] + (hk**3 - 3*hk*s**2 + 2*s**3)/(hk**3)*v[low_x_index] + (s**2*(s - hk))/(hk**2)*slopes[low_x_index + 1] + (s*(s - hk)**2)/(hk**2) * slopes[low_x_index]
-        else: # If xq is outside x, set as ending value of data
-            vq[i] = v[-1]
-
+        hk = x[low_x_index + 1] - x[low_x_index] # Interval length
+        s = xq[i] - x[low_x_index]         
+        vq[i] = (3*hk*s**2 - 2*s**3)/(hk**3)*v[low_x_index + 1] + (hk**3 - 3*hk*s**2 + 2*s**3)/(hk**3)*v[low_x_index] + (s**2*(s - hk))/(hk**2)*d[low_x_index + 1] + (s*(s - hk)**2)/(hk**2) * d[low_x_index]
+            
+    vq[np.where(np.greater(xq, x[-1]))] = v[-1] # Set vq at xq > x[end] to v[end]
+    # print("Values calc took ", time.perf_counter() - time2, " s")            
     return vq
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
